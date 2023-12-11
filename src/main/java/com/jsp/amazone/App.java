1a4f6f3ddecb15e845d1540515225776b03c5473
unknown <krishnanshupauranik@gmail.com>
2023-12-11 14:51:47 +0530

    one changed made

---
+++ src/main/java/com/jsp/amazone/App.java
@@ -0,0 +1,17 @@
+package com.jsp.amazone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        // change 1 by kanha
+//        new changed made.
+        
+    }
+
+}
